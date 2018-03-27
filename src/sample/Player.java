da5c0a7b8071b9a09017d20688aa6a0cc1e57b46
Fereshte Nasirpour <f.nasirpour99@gmail.com>
2018-03-27 14:47:01 +0430

    2ndCommit
    Player circle was set

--- src/sample/Player.java
+++ src/sample/Player.java
@@ -1,4 +1,26 @@
 package sample;
 
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.Node;
+
 public class Player {
+
+        Circle player;
+
+
+        public Player() {
+
+                player = new Circle(50, Color.WHITE);
+                player.setFill(Color.BLACK);
+        } //end constructor
+
+        public void setX(double x) { player.setTranslateX(x); }
+        public void setY(double y) { player.setTranslateY(y); }
+        public double getX() {return player.getTranslateX(); }
+        public double getY() {return player.getTranslateY(); }
+        public Node getNode() {return player; }
+
+
+
 }
